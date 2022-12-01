382e9f27515792732120a593b1203ae87be55f6f
manura18 <mary.kondreskul@gmail.com>
2022-12-01 18:52:10 +0200

    feat(ui): add SInput

---
+++ src/shared/ui/SInput/SInput.vue
@@ -0,0 +1,26 @@
+<template>
+  <div>
+    <label
+      class="
+    text-sm
+    font-bold
+    text-stone-500"
+    >
+      Введіть номер телефону
+    </label>
+  </div>
+  <div>
+    <div class="mt-1">
+      <input
+        class="
+        form-input block
+        min-h-[44px] w-full
+        rounded-none border
+        border-gray-300
+        focus:border-indigo-300/50
+        focus:shadow-[0_0_5px_#ccc]
+        focus:ring-0 sm:text-base"
+      >
+    </div>
+  </div>
+</template>
