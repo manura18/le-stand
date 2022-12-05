60b320b3034e08f4c34bca8c60e3eac33ca49cf4
manura18 <mary.kondreskul@gmail.com>
2022-12-06 01:28:06 +0200

    fix(ui): resolve pull request issues

--- src/shared/ui/SInput/SInput.vue
+++ src/shared/ui/SInput/SInput.vue
@@ -1,26 +1,14 @@
 <template>
-  <div>
-    <label
-      class="
-    text-sm
-    font-bold
-    text-stone-500"
-    >
-      Введіть номер телефону
-    </label>
+  <div
+    class="text-sm font-bold text-stone-500"
+  >
+    <slot />
   </div>
-  <div>
-    <div class="mt-1">
-      <input
-        class="
-        form-input block
-        min-h-[44px] w-full
-        rounded-none border
-        border-gray-300
-        focus:border-indigo-300/50
-        focus:shadow-[0_0_5px_#ccc]
-        focus:ring-0 sm:text-base"
-      >
-    </div>
-  </div>
+  <input
+    class="
+        form-input mt-1 block min-h-[44px] w-full rounded-none
+        border border-gray-300  focus:border-indigo-300/50
+        focus:shadow-[0_0_5px_#ccc] focus:ring-0 sm:text-base
+        "
+  >
 </template>
